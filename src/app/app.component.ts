a679cc4e3acad16a00be0de7b286a8af1119b4c8
emvalencaf <edsonmvf@gmail.com>
2024-06-04 17:18:46 -0300

    :sparkles: feat: Initial page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'frontend';
+  contact = {
+    firstName: "Anthony",
+    lastName: "O'Charle",
+    email: "aocharle@gmail.com",
+    birthday: "1970-04-01",
+    isFavorite: true,
+    phones: [
+      {
+        number: "123456789",
+        codeArea: "81",
+        isTelegram: false,
+        isWhatsapp: true,
+      },
+      {
+        number: "987654321",
+        codeArea: "81",
+        isTelegram: true,
+        isWhatsapp: false,
+      }
+    ],
+  }
 }
